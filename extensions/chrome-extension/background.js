aa062e2d45173c9b393a89a9cbd3340f44423230
nahurd94 <nahurd@gmail.com>
2015-04-18 20:58:52 -0400

    switch statement set up
    checks for each key and sends an alert

--- extensions/chrome-extension/background.js
+++ extensions/chrome-extension/background.js
@@ -2,8 +2,20 @@
 //301 is the value for the alt key
 
 function recieveMessage(request, sender, sendResponse) {
-	var hotkey = request.hotkey;
+	var hotkey = request.hotkey.substring(7,9);
 	console.log("Hotkey: " + hotkey);
+	
+	switch(hotkey){
+		case "65": //go to about us
+		alert("About Us");
+		break;
+		case "72": //go to home page
+		alert("Home");
+		break;
+		case "67": //go to contact us
+		alert("Contact Us");
+		break;
+	}
 }
 
 chrome.runtime.onMessage.addListener(recieveMessage);
