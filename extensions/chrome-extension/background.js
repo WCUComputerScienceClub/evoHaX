8cc208526001033fbd568a802036e0e0d970f630
nahurd94 <nahurd@gmail.com>
2015-04-18 21:06:34 -0400

    handled merge conflict in background.js

--- extensions/chrome-extension/background.js
+++ extensions/chrome-extension/background.js
@@ -1,17 +1,27 @@
-<<<<<<< HEAD
+
 // receive message from content script
-=======
 var tabs = [];
 
 var previousDomain = "";
 
 // recieve message from content script
->>>>>>> origin/master
 //301 is the value for the alt key
 
 function recieveMessage(request, sender, sendResponse) {
-	var hotkey = request.hotkey;
+	var hotkey = request.hotkey.substring(7,9);
 	console.log("Hotkey: " + hotkey);
+	
+	switch(hotkey){
+		case "65": //go to about us
+		alert("About Us");
+		break;
+		case "72": //go to home page
+		alert("Home");
+		break;
+		case "67": //go to contact us
+		alert("Contact Us");
+		break;
+	}
 }
 
 chrome.runtime.onMessage.addListener(recieveMessage);
